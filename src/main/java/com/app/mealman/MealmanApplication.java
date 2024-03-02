abc751e197ebb592475e5a04917bdb913ceca382
akyuumi <akyuumi.pg@gmail.com>
2024-03-02 10:10:02 +0900

    first commit

---
+++ src/main/java/com/app/mealman/MealmanApplication.java
@@ -0,0 +1,13 @@
+package com.app.mealman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealmanApplication.class, args);
+	}
+
+}
