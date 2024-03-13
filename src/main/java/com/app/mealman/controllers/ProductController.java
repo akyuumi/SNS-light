76ce8d641ad3a33a39d4501c1bdbc6ff64ffb933
akyuumi <akyuumi.pg@gmail.com>
2024-03-13 17:09:25 +0900

    build.gradleを修正

--- src/main/java/com/app/mealman/controllers/ProductController.java
+++
@@ -1,7 +0,0 @@
-package com.app.mealman.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController("/products")
-public class ProductController {
-}
---
+++ src/main/java/com/app/mealman/entities/User.java
@@ -0,0 +1,19 @@
+//package com.app.mealman.entities;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//@Entity // JPAのエンティティであることを示します。
+//public class User {
+//
+//    @Id // 主キーであることを示します。
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) // IDの生成戦略をデータベースに依存する自動増分に設定します。
+//    private Long id;
+//
+//    private String name;
+//
+//    private String email;
+//
+//}
