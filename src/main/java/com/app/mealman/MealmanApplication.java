78b590558b07b0f66b87a5284be2b3e77760c75d
akyuumi <akyuumi.pg@gmail.com>
2024-04-26 21:45:09 +0900

    ロガーの追加

--- src/main/java/com/app/mealman/MealmanApplication.java
+++ src/main/java/com/app/mealman/MealmanApplication.java
@@ -1,5 +1,7 @@
 package com.app.mealman;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MealmanApplication {
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(MealmanApplication.class);
+		logger.info("Hello Logback!!");
 		SpringApplication.run(MealmanApplication.class, args);
 	}
 
